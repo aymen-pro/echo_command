16173973d63bab130e0033e2b47a236b96f3397f
Aymen <aymendd3131@gmail.com>
2023-07-05 21:47:38 +0100

    Get cli args

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use std::env;
+
+fn main() {
+    let cli_args = get_cli_args();
+}
+
+fn get_cli_args() -> Vec<String> {
+    let mut cli_args: Vec<_> = env::args_os().collect();
+    let cli_args = cli_args.split_off(1);
+
+    cli_args
+        .iter()
+        .map(|arg| {
+            arg.to_str()
+                .expect("arguments should contain valid unicode values")
+                .to_string()
+        })
+        .collect()
+}
